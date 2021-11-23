2fa6f959f862a5a51bccf82b5444799ddad75b17
“hoangvanhoa1701” <hoangvanhoa1701@gmail.com>
2021-11-23 14:50:23 +0700

    feat: add flash message and connect mysql2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   # GET /about
   get "about", to: "about#index"
+  # get "about-us", to: "about#index", as: :about
 
-  get "/", to: "main#index"
-
-
+  root to: "main#index"
+  # get "/", to: "main#index"
 end
